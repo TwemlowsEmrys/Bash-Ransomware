4d929d27a3091199c61a9aba184f82b43b89ad62
Twemlow <79366364+TwemlowsEmrys@users.noreply.github.com>
2023-01-18 14:55:22 +0000

    MagnusDecryptor.sh

--- MagnusDecryptor.sh
+++ MagnusDecryptor.sh
@@ -1,33 +1,60 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-    echo "you must have root perms to run this script!"
-    exit 1
-fi
+check_root() {
+    if [[ $EUID -ne 0 ]]; then
+        echo "you must have root perms to run this script!"
+        exit 1
+    fi
+}
 
-if ! [ -x "$(command -v gpg)" ]; then
-    echo 'Error: gpg is not installed. Installing gpg...'
-    apt-get update && apt-get install -y gnupg
-    echo 'gpg has been installed'
-else
-    echo 'gpg is already installed'
-fi
+install_gpg() {
+    if ! [ -x "$(command -v gpg)" ]; then
+        echo 'Error: gpg is not installed. Installing gpg...'
+        apt-get update && apt-get install -y gnupg
+        echo 'gpg has been installed'
+    else
+        echo 'gpg is already installed'
+    fi
+}
 
-read -p "Enter the password for decryption: " password
+get_password() {
+    read -p "Enter the password for decryption: " password
+}
 
-echo "Decryption in progress..."
+check_password() {
+    for target_dir in "${target_dirs[@]}"; do
+        for file in $(find $target_dir -type f -name "*.Magnus"); do
+            echo "$password" | gpg --batch --yes --passphrase-fd 0 --list-packets "$file" &> /dev/null
+            if [ $? -ne 0 ]; then
+                echo "Incorrect password provided, exiting..."
+                exit 1
+            else
+                echo "Decryption in progress..."
+                return 0
+            fi
+        done
+    done
+}
 
-target_dir_1="/var/lib/docker/volumes"
-target_dir_2="/var/lib/docker/containers"
-target_dir_3="/home"
-target_dir_4="/var/www"
-target_dir_5="/home/backups"
-target_dir_6="/var/backups"
+decrypt_files() {
+    target_dirs=("/var/lib/docker" "/home" "/var/www" "/home/backups" "/var/backups")
 
-for file in $(find $target_dir_1 $target_dir_2 $target_dir_3 $target_dir_4 $target_dir_5 $target_dir_6 -type f -name "*.Magnus"); do
-    echo "$password" | gpg --batch --yes --passphrase-fd 0 --decrypt "$file"
-    rm "$file"
-    mv "${file%.Magnus}" "$file"
-done
+    for target_dir in "${target_dirs[@]}"; do
+        for file in $(find $target_dir -type f -name "*.Magnus"); do
+            echo "$password" | gpg --batch --yes --passphrase-fd 0 --decrypt "$file"
+            rm "$file"
+            mv "${file%.Magnus}" "$file"
+        done
+    done
+    echo "Decryption has finished!"
+}
 
-echo "Decryption has finished!"
+main() {
+    check_root
+    install_gpg
+    get_password
+    check_password
+    decrypt_files
+}
+
+main
